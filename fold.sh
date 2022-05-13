8588cfc2c6e6c06c2e2bae4df760bbded64fbe33
PKTUser <102319653+pktuser@users.noreply.github.com>
2022-05-12 20:00:11 -0600

    Update fold.sh

--- fold.sh
+++ fold.sh
@@ -10,18 +10,19 @@ read -p "press enter to continue, ctrl-c to quit" entr
 clear
 
 loadlog() {
-    PS3="Select 1 - yes or 2 - no: "
+    PS3="Select 1 - yes, 2 - no, 3 - delete log: "
     echo "Load saved setting from file?"
         select yn in "Yes" "No"; do
         case $yn in
             Yes ) break;;
             No ) promptuser; break;;
+            Delete ) rm -rf fold.log; break;;
         esac
     done
 
     mapfile -t log < fold.log
     clear
-    printf "\n\n loading log file . . .\n\n"
+    printf "\n\nLog file loaded, as follows:\n\n"
     echo "path:${log[0]}"
     echo "address: ${log[1]}"
     echo "password: ${log[2]}"
@@ -29,14 +30,15 @@ loadlog() {
 }
 
 promptuser() {
+    clear
     read -p "/path/to/pktctl (eg /bin/pktctl): " pktctl
     read -p "Wallet address: " addr
     read -p "Wallet passphrase: " pass
     printf "\n\n"
     echo "Would you like to save these settings and your password in a local unencrypted file?"
     echo "Please note this is very insecure and puts you at risk of theft if anyone accesses this file"
-
-    read -p " type \"yes\" to save (insecure), type \"no\" to continue without saving" yn
+    printf "\n"
+    read -p "Type \"yes\" to save (insecure), type \"no\" to continue without saving: " yn
 #        select yn in "yes" "no"; do
         case $yn in
             yes ) printf "$pktctl\n$addr\n$pass" > fold.log; break;;
