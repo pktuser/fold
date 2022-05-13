efac2b78daf1425560ebcae1001a8b5f81ee47d8
PKTUser <102319653+pktuser@users.noreply.github.com>
2022-05-13 12:42:11 -0600

    Update fold.sh

--- fold.sh
+++ fold.sh
@@ -48,16 +48,17 @@ promptuser() {
     read -p "/path/to/pktctl (eg /bin/pktctl): " pktctl
     read -p "Wallet address: " addr
     read -p "Wallet passphrase: " pass
-    printf "\n\n"
-    echo "Would you like to save these settings and your password in a local unencrypted file?"
-    printf "Please note this is ${RED}very insecure${NF} and puts you at ${RED}risk of theft${NF} if anyone accesses this file\n"
-    printf "\n"
-    read -p "Type \"yes\" to save (insecure), type \"no\" to continue without saving: " yn
         while true; do
+            clear
+            printf "\n\n"
+            echo "Would you like to save these settings and your password in a local unencrypted file?"
+            printf "Please note this is ${RED}very insecure${NF} and puts you at ${RED}risk of theft${NF} if anyone accesses this file\n"
+            printf "\n"
+            read -p "Type \"yes\" to save (insecure), type \"no\" to continue without saving: " yn
         case $yn in
             yes ) printf "$pktctl\n$addr\n$pass" > fold.log; break;;
             no ) break;;
-            * ) echo "type yes or no";;
+            * ) echo "type yes or no"; sleep 1; break;;
         esac
         done
 }
