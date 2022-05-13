aecfce3fc1c783f07ce81d38f7e9f7906e22075e
PKTUser <102319653+pktuser@users.noreply.github.com>
2022-05-12 20:53:54 -0600

    Update fold.sh

--- fold.sh
+++ fold.sh
@@ -68,8 +68,8 @@ sleep 1
 x=0
 while true
 do
-     # curl https://explorer.pkt.cash/api/v1/PKT/pkt/address/pkt1q9dczv9ne8mfg98aya90kepflk2j2whhfqqn0mk | grep balanceCount | awk '{print $2;}' | tr -d ',' 
-     utx=`curl -s $pullurl$addr | grep balanceCount | awk '{print $2;}' | tr -d ','`
+     eval curl https://explorer.pkt.cash/api/v1/PKT/pkt/address/pkt1q9dczv9ne8mfg98aya90kepflk2j2whhfqqn0mk | grep balanceCount | awk '{print $2;}' | tr -d ',' > utx
+     # utx=`curl -s $pullurl$addr | grep balanceCount | awk '{print $2;}' | tr -d ','`
      echo $pullurl
      echo $addr
      echo "utx value: $utx"
